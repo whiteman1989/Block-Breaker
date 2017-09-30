7b9779a1a31b855cc41ac4f1a7c8798d64dae761
Andrey Gorban <whiteman1989@gmail.com>
2017-09-30 21:04:38 +0300

    Start work

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name){
+		Debug.Log ("Load level requested for: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest (){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+}
