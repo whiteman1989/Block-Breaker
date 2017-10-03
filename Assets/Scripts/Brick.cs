723c98a8b8953b0c4347b1857c2cf7fb23c05f5e
Andrey Gorban <whiteman1989@gmail.com>
2017-10-04 00:12:45 +0300

    Adding the SimulationWin method and NextLevel

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -6,18 +6,24 @@ public class Brick : MonoBehaviour {
 	public int maxHits;
 	public int timesHit;
 
+	private LevelManager levelManager;
+
 	// Use this for initialization
 	void Start () {
 		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
 	
 	}
 
 	void OnCollisionEnter2D (Collision2D col) {
 		timesHit ++;
+		SimulateWin ();
+	}
+
+	// TODO Remove this method once we can actually win!
+	void SimulateWin (){
+		levelManager.LoadNextLevel ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -13,4 +13,7 @@ public class LevelManager : MonoBehaviour {
 		Application.Quit ();
 	}
 
+	public void LoadNextLevel () {
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
 }
