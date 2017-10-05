702294129f7e246ef2d204ca3ab873ad149964be
Andrey Gorban <whiteman1989@gmail.com>
2017-10-05 21:59:34 +0300

    bit ready

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
+	public float adjustingMult = 4f;
+	public float tweakMult = 1f;
+	public float maxSpeed = 17f;
+
 	private Paddle paddle;
 	private bool hasStarted = false;
 	private Vector3 paddleToBallVector;
@@ -18,6 +22,14 @@ public class Ball : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D (Collision2D col){
+
+		TweakVelociti ();
+
+
+		if (col.gameObject.tag == "Paddle") {
+			TweakAngle();
+		}
+
 		if (!(col.gameObject.tag == "Breakable"))
 		audio.Play ();
 	}
@@ -30,11 +42,40 @@ public class Ball : MonoBehaviour {
 
 			//Wait for mouse press to lunch 
 			if (Input.GetMouseButtonDown (0)) {
-				hasStarted = true;
-				this.GetComponent<Rigidbody2D>().simulated = true;
-				this.rigidbody2D.velocity = new Vector2 (2f, 10f);
+				StartTheBall ();
 			}
 		}
 
+	}
+
+
+
+
+	void TweakVelociti (){
+		if (rigidbody2D.velocity.magnitude <= maxSpeed) {
+			Vector2 tweak = rigidbody2D.velocity * Random.Range (0f, 0.05f) * tweakMult;
+			rigidbody2D.velocity += tweak;
+		}
+		rigidbody2D.velocity = Rotate (this.rigidbody2D.velocity, Random.Range(0f,0.1f));
+		print ("Velocity = " + rigidbody2D.velocity.magnitude);
+	}
+
+	void TweakAngle(){
+		float adjusting = Mathf.Clamp( (paddle.transform.position.x - this.transform.position.x),-0.5f,0.5f)*adjustingMult;
+		this.rigidbody2D.velocity = Rotate (this.rigidbody2D.velocity, adjusting);
+	}
+
+	Vector2 Rotate(Vector2 aPoint, float aDegree)
+	{
+		float rad = aDegree * Mathf.Deg2Rad;
+		float s = Mathf.Sin(aDegree);
+		float c = Mathf.Cos(aDegree);
+		return new Vector2( aPoint.x * c - aPoint.y * s, aPoint.y * c + aPoint.x * s);
+	}
+
+	void StartTheBall (){
+		hasStarted = true;
+		this.GetComponent<Rigidbody2D>().simulated = true;
+		this.rigidbody2D.velocity = new Vector2 (2f, 10f);
 	}
 }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,19 +3,35 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
+	public bool autoPlay = false;
+
+	private Ball ball;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		ball = GameObject.FindObjectOfType<Ball>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (!autoPlay) {
+			MoveWithMouse ();
+		} else {
+			AutoPlay ();
+		}
+	}
+
+	void MoveWithMouse (){
 		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y , 0f);
-
 		float mousePositionBlocks = Input.mousePosition.x / Screen.width * 16;
-
 		paddlePos.x = Mathf.Clamp(mousePositionBlocks , 0.5f , 15.5f);
+		this.transform.position = paddlePos;
+	}
 
+	void AutoPlay (){
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y , 0f);
+		paddlePos.x = Mathf.Clamp(ball.transform.position.x , 0.5f , 15.5f);
 		this.transform.position = paddlePos;
 	}
 }
